71a9ea70a717b78a95785c50a6902b1488df12ea
mahavarshini086 <varshinimuthukumaran2011@gmail.com>
2024-08-20 15:57:05 +0530

    Add files via upload

---
+++ varsh/ash/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+class betacls(APIView):
+      def post(self):
+          return Response("Hey Buddy")
+
---
+++ varsh/varsh/__init__.py
@@ -0,0 +1,2 @@
+from .initialize_model import modelpredictor
+model = modelpredictor()
